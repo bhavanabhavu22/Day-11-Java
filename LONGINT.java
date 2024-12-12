42f3070f4ea85fc753bcd98917975e07aa1de8e2
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 17:42:22 +0530

    Add files via upload

---
+++ LONGINT.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class LONGINT {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        long ans=45;
+        long s=sc.nextInt();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+    
+}
