f8f338c054b683530f0ddd94ffa6c222f9f2c3ca
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 21:44:48 +0530

    Add files via upload

---
+++ LONGUSE.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class LONGUSE
+{
+    public static void main(String[] args)
+    {
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        //accepting long
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
