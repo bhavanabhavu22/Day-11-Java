bf4e9d0f1fde3b1defb62e6fa8f483b7e4c676d7
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 17:36:37 +0530

    Add files via upload

---
+++ COLLECTIONuse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class COLLECTIONuse {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(345);
+        a.add(45);
+        a.add(67);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+}
