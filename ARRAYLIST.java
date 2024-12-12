080261e3d229224e332930f3dcc479291caa196c
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 14:02:15 +0530

    Add files via upload

---
+++ ARRAYLIST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ARRAYLIST
+{
+    public static void main(String arsg[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(23);
+        a.add(456);
+        a.add(45);
+        System.out.println(a);
+    
+    
+    }
+}
